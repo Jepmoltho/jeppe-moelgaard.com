5796aa7130bc1e82c3d352e75802cd14721aa803
Jepmoltho <jept@itu.dk>
2023-07-25 20:13:31 +0200

    media folder and image rendering

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,29 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-//import React, { useEffect } from "react";
 import "../App.css";
 import Typer from "./Typer";
+import myImage from "../media/logo192.png";
 
 export default function Header() {
-  /*
-    useEffect(() => {
-    const jumpingletters =
-      document.getElementsByClassName("jumping-letters")[0];
-    const arrayOfJumpingLetters = jumpingletters.innerText.split("");
-    //console.log(arrayOfJumpingLetters);
-  }, []);
-*/
   return (
     <header className="header">
       <div className="container">
         <div className="row">
-          <div className="col m-5">
-            <p className="text-start mb-1">Hi, I'm a</p>
-            <h1 className="text-start">
-              <Typer text="Frontend Developer" delay={70} start={0} />
+          <div className="col-7 m-5">
+            <p className="text-start tagline mb-1">Hi, I'm a</p>
+            <h1 className="text-start headline">
+              <Typer text="Frontend Developer" delay={50} start={0} />
               <br />
             </h1>
-            <h3 className="text-start">
+            <h3 className="text-start headline">
               <Typer
                 text="skilled in coding and communications"
-                delay={50}
-                start={1.8}
+                delay={30}
+                start={1}
               />
             </h3>
-
-            {/*<h1 className="jumping-letters">Frontend developer</h1>*/}
-            {/*<h1>+</h1>*/}
-            {/*<h2>communications specialist</h2>*/}
           </div>
-          <div className="col">
+          <div className="col paragraph">
+            <img src={myImage} alt="profile"></img>
             <p>test</p>
           </div>
         </div>
