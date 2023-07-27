fd36ebdeb745b96945628138ad369ba7e281cfbe
Jepmoltho <jept@itu.dk>
2023-07-27 13:02:57 +0200

    Circle animation halfway done

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../App.css";
 
 export default function Bio() {
@@ -7,6 +7,8 @@ export default function Bio() {
     "As a Frontend Developer with a Master's Degree in Software Development from ITU and a Bachelor's Degree in Journalism from DMJX, I bring a unique blend of technical and communication skills to the table.",
     "My technical experience revoScript, and React, alongside other frontend frameworks and technologies.",
     "In a professional capacity, I have created web applications for enterprises, focusing on data management and visualisation.",
+    "Lorem ipsum this is slide 4",
+    "Fade out. Mic drop",
   ];
 
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -15,6 +17,22 @@ export default function Bio() {
     setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
   };
 
+  //prettier-ignore
+  useEffect(() => {
+    const circlesToFill = document.getElementsByClassName("ci" + currentSlide);
+    Array.from(circlesToFill).forEach(circle => circle.style.backgroundColor = "#4ca1af");
+  
+    if (currentSlide === 0) {
+      const allCircles = document.querySelectorAll("[class^='ci']");
+      Array.from(allCircles).forEach(circle => {
+        if (!circle.classList.contains("ci0")) {
+          circle.style.removeProperty("background-color");
+        }
+      });
+    }
+
+  }, [currentSlide]);
+
   return (
     <header className="bio">
       <div className="container">
@@ -22,21 +40,21 @@ export default function Bio() {
           <div className="col-md-5">
             <div className="d-flex align-items-center justify-content-center h-100">
               <div>
-                <div className="circle c1 ci1" />
+                <div className="circle c1 ci0" />
               </div>
               <div>
-                <div className="circle c2 ci2" />
-                <div className="circle c3 ci2" />
+                <div className="circle c2 ci1" />
+                <div className="circle c3 ci1" />
               </div>
               <div>
-                <div className="circle c4 ci3" />
+                <div className="circle c4 ci2" />
               </div>
               <div>
-                <div className="circle c5 ci4" />
-                <div className="circle c6 ci4" />
+                <div className="circle c5 ci3" />
+                <div className="circle c6 ci3" />
               </div>
               <div>
-                <div className="circle c7 ci5" />
+                <div className="circle c7 ci4" />
               </div>
             </div>
           </div>
@@ -56,20 +74,3 @@ export default function Bio() {
     </header>
   );
 }
-/*
-  const [slide, setSlide] = useState();
-
-  let index = 1;
-  function changeSlide(index) {
-    index += 1;
-  }
-
-        As a Frontend Developer with a Master's Degree in Software
-                  Development from ITU and a Bachelor's Degree in Journalism
-                  from DMJX, I bring a unique blend of technical and
-                  communication skills to the table. My technical experience
-                  revoScript, and React, alongside other frontend frameworks and
-                  technologies. In a professional capacity, I have created web
-                  applications for enterprises, focusing on data management and
-                  visualisation.
-  */
