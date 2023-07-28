69b0e1bb87857763b45703e4cb1b665b1f459fe8
Jepmoltho <jept@itu.dk>
2023-07-28 11:26:19 +0200

    Portfolio component basics done

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,12 +1,12 @@
 //import myImage from "../media/profile-picture-v2.png";
 import js from "../media/JS.png";
 import html from "../media/HTML.png";
-import aspnet from "../media/aspnet.png";
-import bootstrap from "../media/bootstrap.png";
-import csharp from "../media/csharp.png";
-import css from "../media/CSS.png";
-import github from "../media/github.png";
-import umbraco from "../media/umbraco.png";
+import net from "../media/net-v2.png";
+import bootstrap from "../media/bootstrap-v3.png";
+import csharp from "../media/csharp-v2.png";
+import css from "../media/CSS-v3.png";
+import github from "../media/github-v2.png";
+import umbraco from "../media/umbraco-v2.png";
 
 export default function Project({ tagline, headline, description, image }) {
   return (
@@ -19,6 +19,15 @@ export default function Project({ tagline, headline, description, image }) {
                 <p className="tagline-tabs">{tagline}</p>
                 <h2>{headline}</h2>
                 <p>{description}</p>
+                <div className="links">
+                  <p className="inline-p">Links: </p>
+                  <p className="inline-p">Pricing, </p>
+                  <p className="inline-p">Industries, </p>
+                  <p className="inline-p">Front page, </p>
+                  <p className="inline-p">Demo, </p>
+                  <p className="inline-p">Blog, </p>
+                  <p className="inline-p">Blog post, </p>
+                </div>
               </div>
             </div>
           </div>
@@ -29,16 +38,17 @@ export default function Project({ tagline, headline, description, image }) {
           </div>
         </div>
         <div className="techstack">
-          <p className="paragraphtechstack">
-            Technologies:
-            <img className="techlogo" src={js} />
-            <img className="techlogo" src={html} />
-            <img className="techlogo" src={css} />
-            <img className="techlogo" src={bootstrap} />
-            <img className="techlogo" src={aspnet} />
-            <img className="techlogo" src={csharp} />
-            <img className="techlogo" src={umbraco} />
-          </p>
+          <p className="paragraphtechstack">Techstack:</p>
+          <div>
+            <img className="techlogo" alt="js logo" src={js} />
+            <img className="techlogo" alt="html logo" src={html} />
+            <img className="techlogo" alt="css logo" src={css} />
+            <img className="techlogo" alt="bootstrap logo" src={bootstrap} />
+            <img className="techlogo" alt=".net logo" src={net} />
+            <img className="techlogo" alt="c# logo" src={csharp} />
+            <img className="techlogo" alt="umbraco logo" src={umbraco} />
+            <img className="techlogo" alt="github logo" src={github} />
+          </div>
         </div>
       </div>
     </div>
