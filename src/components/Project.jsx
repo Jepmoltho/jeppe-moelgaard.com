c8dd976a6d170ba5a5436b984ea94db70ac3e0a8
Jepmoltho <jept@itu.dk>
2023-07-31 13:45:42 +0200

    text content draft for bio

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -27,7 +27,6 @@ export default function Project({
   const isVisible = useScrollHook(animationContainerRef);
 
   useEffect(() => {
-    //console.log("isVisible: ", isVisible);
     if (isVisible) {
       document
         .getElementsByClassName("animation-container")
