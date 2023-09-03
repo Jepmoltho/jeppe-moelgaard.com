4d513ee4e85b05d79b4e1031c10292063f06a8e0
Jepmoltho <jept@itu.dk>
2023-09-03 17:53:15 +0200

    Added rest of journalism content

--- src/data/journalism.jsx
+++ src/data/journalism.jsx
@@ -7,11 +7,27 @@ import hack2 from "../media/Journalism media/hack2.png";
 import hack3 from "../media/Journalism media/hack3.png";
 import lasertruck from "../media/Journalism media/lasertruck.png";
 import nano from "../media/Journalism media/nano.png";
-import pks from "../media/Journalism media/pks.png";
-//import pks2 from "../media/Journalism media/pks2.png";
+//import pks from "../media/Journalism media/pks.png";
+import pks3 from "../media/Journalism media/pks3.jpeg";
 import snake from "../media/Journalism media/snake.png";
 import solceller from "../media/Journalism media/solceller.png";
 import vindturbine from "../media/Journalism media/vindturbine.png";
+// import kpmg1 from "../media/Journalism media/kpmg1.jpg";
+// import kpmg2 from "../media/Journalism media/kpmg2.jpg";
+import kpmg3 from "../media/Journalism media/kpmg3.jpg";
+//import kpmg4 from "../media/Journalism media/kpmg4.jpg";
+// import kpmg5 from "../media/Journalism media/kpmg5.jpg";
+import kpmg6 from "../media/Journalism media/kpmg6.jpg";
+import kpmg7 from "../media/Journalism media/kpmg7.jpg";
+import kpmg8 from "../media/Journalism media/kpmg8.jpg";
+import nba1 from "../media/Journalism media/nba1.jpg";
+import nba2 from "../media/Journalism media/nba2.jpg";
+import nba3 from "../media/Journalism media/nba3.jpg";
+import sciencereport1 from "../media/Journalism media/sciencereport1.jpg";
+import sciencereport2 from "../media/Journalism media/sciencereport2.jpg";
+import sciencereport3 from "../media/Journalism media/sciencereport3.jpg";
+import sciencereport4 from "../media/Journalism media/sciencereport4.jpg";
+import dtupaper from "../media/Journalism media/dtu.jpg";
 
 export const journalism = [
   {
@@ -34,6 +50,33 @@ export const journalism = [
   },
   {
     id: 2,
+    link: "https://kpmg.com/dk/en/home/media/press-releases/2020/04/artificial-intelligence-prevents-fraud.html",
+    name: "aifraud",
+    title: "Artificial intelligence prevents fraud",
+    image: kpmg3,
+    publisher: "KPMG",
+    date: "21-04-2020",
+  },
+  {
+    id: 3,
+    link: "https://sciencereport.dk/samfund/forskere-fravaelger-at-goere-artikler-frit-tilgaengelige/",
+    name: "sr1",
+    title: "Scientists avoids making articles freely available",
+    image: sciencereport3,
+    publisher: "Science Report",
+    date: "23-02-2020",
+  },
+  {
+    id: 4,
+    link: "https://jeppemoelgaardcom.files.wordpress.com/2020/11/makerdao-article-full.png",
+    name: "MakerDAO",
+    title: "MakerDAO releases multi-collateral DAI",
+    image: nba2,
+    publisher: "Nordic Blockchain",
+    date: "09-09-2019",
+  },
+  {
+    id: 5,
     link: "http://emagstudio.win.dtu.dk/e-books/Dynamo/DYNAMO_54/page_14.html",
     name: "hack3",
     title: "The cyber threat can be turned to our advantage",
@@ -42,7 +85,34 @@ export const journalism = [
     date: "01-09-2019",
   },
   {
-    id: 3,
+    id: 6,
+    link: "https://kpmg.com/dk/en/home/insights/2020/04/quantum-hub-at-kpmg-denmark.html",
+    name: "Quantum",
+    title: "Global Quantum Hub opens in Denmark",
+    image: kpmg8,
+    publisher: "KPMG",
+    date: "01-12-2020",
+  },
+  {
+    id: 7,
+    link: "https://jeppemoelgaardcom.files.wordpress.com/2020/11/htk-artikel-full.png",
+    name: "htk",
+    title: "Municipality invests in blockchain",
+    image: nba3,
+    publisher: "Nordic Blockchain",
+    date: "01-05-2019",
+  },
+  {
+    id: 8,
+    link: "https://sciencereport.dk/samfund/forskningsartikler-udgives-i-strid-med-danmarks-strategi/    ",
+    name: "sr2",
+    title: "Researchers defies Denmark's OA strategy",
+    image: sciencereport4,
+    publisher: "Science Report",
+    date: "20-02-2020",
+  },
+  {
+    id: 9,
     link: "http://emagstudio.win.dtu.dk/e-books/Dynamo/DYNAMO_52/page_5.html",
     name: "vindturbine",
     title: "Worlds largeest wind turbine",
@@ -51,7 +121,25 @@ export const journalism = [
     date: "01-03-2019",
   },
   {
-    id: 4,
+    id: 10,
+    link: "https://kpmg.com/dk/en/home/insights/2020/07/how-will-denmark-get-ready-for-self-driving-cars-.html    ",
+    name: "Denmark prepare for driverless cars",
+    title: "Denmark prepare for driverless cars",
+    image: kpmg7,
+    publisher: "KPMG",
+    date: "08-07-2020",
+  },
+  {
+    id: 11,
+    link: "https://jeppemoelgaardcom.files.wordpress.com/2020/11/ibm-artikel-full.png",
+    name: "supplychain",
+    title: "Retailers track products with blockchain",
+    image: nba1,
+    publisher: "Nordic Blockchain",
+    date: "04-11-2019",
+  },
+  {
+    id: 12,
     link: "http://emagstudio.win.dtu.dk/e-books/DTU/DYNAMO55/page_15.html",
     name: "solceller",
     title: "Tandem solar cells needs to be sustainable",
@@ -60,25 +148,16 @@ export const journalism = [
     date: "01-12-2019",
   },
   {
-    id: 5,
-    link: "http://emagstudio.win.dtu.dk/e-books/Dynamo/DYNAMO_54/page_24.html",
-    name: "hack2",
-    title: "Students learn hacking",
-    image: hack2,
-    publisher: "Dynamo",
-    date: "01-09-2019",
+    id: 13,
+    link: "https://sciencereport.dk/forside/flere-penge-paa-vej-dansk-energiforskning/",
+    name: "sc3",
+    title: "Money on the way to Danish energy research",
+    image: sciencereport2,
+    publisher: "Science Report",
+    date: "27-08-2017",
   },
   {
-    id: 6,
-    link: "http://emagstudio.win.dtu.dk/e-books/Dynamo/dynamo56/page_33.html",
-    name: "lasertruck",
-    title: "Lasertrucks maintain the roads",
-    image: lasertruck,
-    publisher: "Dynamo",
-    date: "03-09-2019",
-  },
-  {
-    id: 7,
+    id: 14,
     link: "http://emagstudio.win.dtu.dk/e-books/DTU/DYNAMO_57/page_33.html",
     name: "nano",
     title: "Startup moves nano analysis out of the lab",
@@ -87,16 +166,70 @@ export const journalism = [
     date: "01-06-2019",
   },
   {
-    id: 8,
+    id: 15,
+    link: "https://pro.ing.dk/datatech/holdning/ai-skal-udvikles-baeredygtigt-minimere-miljoeaftryk",
+    name: "kpmgklummme",
+    title: "AI needs sustainable development",
+    image: kpmg6,
+    publisher: "KPMG / The Engineer",
+    date: "25-06-2020",
+  },
+  {
+    id: 16,
+    link: "https://sciencereport.dk/forside/ingen-energirevolution-hvis-vi-fortsaetter/    ",
+    name: "sc4",
+    title: "No energy revolution if we continue",
+    image: sciencereport1,
+    publisher: "Science Report",
+    date: "27-08-2017",
+  },
+  {
+    id: 17,
+    link: "http://emagstudio.win.dtu.dk/e-books/Dynamo/DYNAMO_54/page_24.html",
+    name: "hack2",
+    title: "Students learn hacking",
+    image: hack2,
+    publisher: "Dynamo",
+    date: "01-09-2019",
+  },
+  {
+    id: 18,
+    link: "http://emagstudio.win.dtu.dk/e-books/Dynamo/dynamo56/page_33.html",
+    name: "lasertruck",
+    title: "Lasertrucks maintain the roads",
+    image: lasertruck,
+    publisher: "Dynamo",
+    date: "03-09-2019",
+  },
+  {
+    id: 19,
     link: "http://emagstudio.win.dtu.dk/e-books/Dynamo/DYNAMO_53/page_31.html",
     name: "pks",
     title: "Autmoatic design system saves millions",
-    image: pks,
+    image: pks3,
     publisher: "Dynamo",
     date: "01-06-2018",
   },
   {
-    id: 9,
+    id: 20,
+    link: "http://emagstudio.win.dtu.dk/e-books/Dynamo/DYNAMO_54/page_17.html",
+    name: "hack1",
+    title: "Spinout makes data cyberimmune",
+    image: hack1,
+    publisher: "Dynamo",
+    date: "01-03-2019",
+  },
+  {
+    id: 21,
+    link: "http://emagstudio.win.dtu.dk/DTU-avisen/DTUavisen18042/page_4.html",
+    name: "Trade energy with blockchain",
+    title: "Trade energy with blockchain",
+    image: dtupaper,
+    publisher: "DTU Paper",
+    date: "01-04-2018",
+  },
+  {
+    id: 22,
     link: "http://emagstudio.win.dtu.dk/e-books/Dynamo/DYNAMO_52/page_40.html",
     name: "snake",
     title: "Students develop snake venom tracking",
@@ -105,16 +238,7 @@ export const journalism = [
     date: "01-03-2019",
   },
   {
-    id: 10,
-    link: "http://emagstudio.win.dtu.dk/e-books/Dynamo/DYNAMO_54/page_16.html",
-    name: "hack1",
-    title: "Spinout makes data cyber immune",
-    image: hack1,
-    publisher: "Dynamo",
-    date: "01-09-2019",
-  },
-  {
-    id: 11,
+    id: 23,
     link: "http://emagstudio.win.dtu.dk/e-books/Dynamo/DYNAMO_54/page_13.html",
     name: "el",
     title: "The electricity grid is undemocratic",
